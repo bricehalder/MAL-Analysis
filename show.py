92b45c30c625d6e1094002e5bf7a54b8afe76b18
Brice Halder <bhalder2@jhu.edu>
2018-06-13 22:46:43 -0700

    finished adding all current shows

--- show.py
+++ show.py
@@ -1,17 +1,24 @@
 from colors import color
+from decimal import Decimal, ROUND_HALF_UP
 
 def aggregate(p, c, a, m, e):
     # Can change the weight of each factor here
     # Currently the highest possible is 10.5 with enjoyment factored in
     # & 10 without it if all input is <= 10
+    if p > 5:
         plot_agg = p * 0.49
         char_agg = c * 0.17
         art_agg = a * 0.17
         vis_agg = m * 0.17
-        # If enjoyment < 5 it will negatively affect the score
-        enjoy_agg = (e - 5) * 0.1
-        agg = plot_agg + char_agg + art_agg + vis_agg + enjoy_agg
-        return agg
+    else:
+        plot_agg = p * 0.7
+        char_agg = c * 0.1
+        art_agg = a * 0.1
+        vis_agg = m * 0.1
+    # If enjoyment < 5 it will negatively affect the score
+    enjoy_agg = (e - 5) * 0.1
+    agg = plot_agg + char_agg + art_agg + vis_agg + enjoy_agg
+    return agg
 
 def header():
     return color.BOLD + '{:30}'.format('NAME') \
@@ -37,10 +44,10 @@ class Show:
         self.sound = m
         self.enjoyment = e
         self.overall = round(aggregate(p,c,a,m,e),2)
+        self.score = Decimal(self.overall).quantize(Decimal('1'), rounding=ROUND_HALF_UP)
 
         # If you want the raw total/10 (takes out enjoyment factor)
         self.raw = round(self.overall - ((self.enjoyment - 5) * 0.1),2)
-
 
     def __str__(self):
         return '{:30}'.format(self.truncate(25)) \
@@ -50,9 +57,9 @@ class Show:
         + '{:<12g}'.format(self.sound) \
         + '{:<12g}'.format(self.enjoyment) \
         + color.RED \
-        + '{:<12g}'.format(self.raw) \
+        + '{:<12}'.format(self.raw) \
         + color.BLUE \
-        + '{:<12g}'.format(self.overall) \
+        + '{:<12}'.format(self.score) \
         + color.END
 
     def __repr__(self):
@@ -68,6 +75,7 @@ class Show:
         self.overall = round(aggregate(self.plot, self.characters, \
         self.visuals, self.sound, self.enjoyment), 2)
         self.raw = round(self.overall - ((self.enjoyment - 5) * 0.1),2)
+        self.score = Decimal(round(self.overall)).quantize(Decimal('1'), rounding=ROUND_HALF_UP)
 
     def set_plot(self, value):
         self.plot = value
